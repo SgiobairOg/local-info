0061521c64b97a6eb9f28d6290181f9533da0f21
Jason Wilson <jason@wilsons.io>
2018-11-10 20:11:11 +0100

    Add UK Template. Add JS Hint Config

---
+++ src/data/uk/index.js
@@ -0,0 +1,14 @@
+export default {
+    "isoCode": {
+        "alpha2": "GB",
+        "alpha3": "GBR"
+    },
+    "unCode": "826",
+    "flag": {
+        "emoji": "🇬🇧"
+    },
+    "name": {
+        "english": "United Kingdom",
+        "local": "United Kingdom"
+    }
+};
